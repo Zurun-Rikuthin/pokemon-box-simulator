9dc8d559e0f0ab411c39d21ab4e72334d1d07a98
Rikuthin <125291285+Zurun-Rikuthin@users.noreply.github.com>
2024-02-17 09:58:23 -0400

    Removed outdated files

--- temp/app.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.app_context().push()
